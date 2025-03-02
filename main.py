ca320dac9053bb401881cfd3741a1e19d24745ad
shymdev <minnich.shyann@gmail.com>
2025-03-02 15:21:58 -0500

    Updated main.py to import modules and outptu html file.

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
-def read_markdown(file_path):
-    """Reads a markdown file and returns its content as a string."""
+from markdown_converter import parser_markdown
+from markdown_converter import render_html
+
+def convert_markdown_to_html(file_path):
+    """Reads a markdown file and returns its as an HTML file."""
+    
+    # Reads a markdown file and returns its content as a string. 
     with open(file_path, 'r') as file:
         return file.read()
+    
+    # Parse the markdown text
+    parsed_text = parse_markdown(md_text)
+    # Render the parsed text as HTML
+    html_output = render_html(parsed_text)
+
+    # Write the HTML output to a file
+    with open('output.html', 'w') as file:
+        file.write(html_output)
+
+    if __name__ == '__main__':
+        convert_markdown_to_html('examples/test_file_1.md')
