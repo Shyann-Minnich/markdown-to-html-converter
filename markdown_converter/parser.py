f2f62b95ef2df52e729df5401f94dbdea938ab25
shymdev <minnich.shyann@gmail.com>
2025-08-10 20:00:55 -0400

    Revised header, list, and blockquote

--- markdown_converter/parser.py
+++ markdown_converter/parser.py
@@ -9,23 +9,38 @@ import re
 def parse_markdown(md_text):
     """Parse Markdown text and return a tuple."""
     parsed_text = []
+    lines = md_text.splitlines()
 
-    for line in md_text:
-        # Check for headings 
+    for line in lines:
+        # Headings
         if line.startswith('######'):
-            parsed_text.append(('h6', line.strip('###### ')))
+            parsed_text.append(('h6', line[6:].strip()))
         elif line.startswith('#####'):
-            parsed_text.append(('h5', line.strip('##### ')))
+            parsed_text.append(('h5', line[5:].strip()))
         elif line.startswith('####'):
-            parsed_text.append(('h4', line.strip('#### ')))
-        elif line.startswith('####'):
-            parsed_text.append(('h4', line.strip('#### ')))
+            parsed_text.append(('h4', line[4:].strip()))
         elif line.startswith('###'):
-            parsed_text.append(('h3', line.strip('### ')))
+            parsed_text.append(('h3', line[3:].strip()))
         elif line.startswith('##'):
-            parsed_text.append(('h2', line.strip('## ')))
+            parsed_text.append(('h2', line[2:].strip()))
         elif line.startswith('#'):
-            parsed_text.append(('h1', line.strip('# ')))
+            parsed_text.append(('h1', line[1:].strip()))
+
+        # Blockquotes
+        elif line.startswith('>'):
+            parsed_text.append(('blockquote', line[1:].strip()))
+
+        # Unordered Lists
+        elif line.startswith('* '):
+            parsed_text.append(('ul', line[2:].strip()))
+        elif line.startswith('- '):
+            parsed_text.append(('ul', line[2:].strip()))
+        elif line.startswith('+ '):
+            parsed_text.append(('ul', line[2:].strip()))
+        
+        # Ordered Lists
+        elif re.match(r'^\d+\.\s', line):
+            parsed_text.append(('ol', re.sub(r'^\d+\.\s', '', line).strip()))
 
         # Bold
         if '**' in line or '__' in line:
@@ -45,18 +60,6 @@ def parse_markdown(md_text):
         # Code Blocks
         if '```' in line:
             parsed_text.append(('pre><code', line.strip('```')))
-
-        # Blockquotes
-        if '>' in line:
-            parsed_text.append(('blockquote', line.strip('>')))
-
-        # Unordered Lists
-        if '*' in line or '-' in line or '+' in line:
-            parsed_text.append(('ul', line.strip('*').strip('-').strip('+')))
-
-        # Ordered LIsts
-        if re.match(r'^\d+\.', line):
-            parsed_text.append(('ol', line.strip('\d.')))
 
         # Links
         if re.match(r'^\[.*\]\(.*\)$', line):
